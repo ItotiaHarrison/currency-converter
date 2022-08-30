6f9c2024b81079130b491e5b7ad2d735705a6561
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-30 14:10:35 +0300

    display rep

--- currency.js
+++ currency.js
@@ -2,31 +2,31 @@ const base = document.getElementById('icurrency').value
 const input = document.querySelector('#currencies')
 
 
-
 function fetchCurrencies() {
     input.addEventListener('submit', function (event) {
         event.preventDefault()
-    })
 
 
-    fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
-        .then(response => response.json())
-        .then(data => {
-            const amount = document.getElementById('amount-input').value;
-            const counterCurrency = document.getElementById('lcurrency').value;
-            const exchangeRate = data.rates[counterCurrency];
-            function convert() {
-                return amount * exchangeRate
-            }
-            document.querySelector('.result').innerHTML 
-        })
-        .catch(error => console.log(error))
+        fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
+            .then(response => response.json())
+            .then(data => {
+                const amount = document.getElementById('amount-input').value;
+                const counterCurrency = document.getElementById('lcurrency').value;
+                const exchangeRate = data.rates[counterCurrency];
+                function convert() {
+                    return amount * exchangeRate
+                }
+
+                document.querySelector('.result').innerHTML = `${amount} ${base.toUpperCase()} equal to  ${convert().toFixed(2)} ${counterCurrency}`;
+
+            })
+            .catch(error => console.log(error))
+    });
 }
-
 
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
     fetchCurrencies();
-})
+});
