5db8611fae37a9bab6b88a20c991de27ef29dbd7
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-30 13:44:26 +0300

    return

--- currency.js
+++ currency.js
@@ -1,26 +1,32 @@
 const base = document.getElementById('icurrency').value
 const input = document.querySelector('#currencies')
-input.addEventListener('submit', function(event) {
-    event.preventDefault()
-    
-})
 
 
-function fetchCurrencies(){
-    fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
-    .then(response => response.json())
-    .then(data => {
-        const amount = document.getElementById('amount-input').value;
-        const counterCurrency = document.getElementById('lcurrency').value;
-        const exchangeRate = data.rates[counterCurrency];
+
+function fetchCurrencies() {
+    input.addEventListener('submit', function (event) {
+        event.preventDefault()
     })
-    .catch(error => console.log(error)) 
+
+
+    fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
+        .then(response => response.json())
+        .then(data => {
+            const amount = document.getElementById('amount-input').value;
+            const counterCurrency = document.getElementById('lcurrency').value;
+            const exchangeRate = data.rates[counterCurrency];
+            function convert() {
+                return amount * exchangeRate
+            }
+            document.querySelector('.result').innerHTML 
+        })
+        .catch(error => console.log(error))
 }
 
 
 
 
 
-document.addEventListener('DOMContentLoaded', function(){
-   fetchCurrencies();
+document.addEventListener('DOMContentLoaded', function () {
+    fetchCurrencies();
 })
